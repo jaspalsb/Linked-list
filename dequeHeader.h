8f09cc571d4c74e6f801efbc685d35ae9c50dbbf
Jaspal Bainiwal <jbainiwal@gmail.com>
2018-09-04 20:10:46 -0700

    Create dequeHeader.h

---
+++ dequeHeader.h
@@ -0,0 +1,33 @@
+#ifndef dequeHeader_H
+#define dequeHeader_H
+
+#ifndef TYPE
+#define TYPE double
+#endif
+
+#ifndef LT
+#define LT(A, B) ((A) < (B))
+#endif
+
+#ifndef EQ
+#define EQ(A, B) ((A) == (B))
+#endif
+
+struct CircularList;
+
+struct CircularList* circularListCreate();
+void circularListDestroy(struct CircularList* list);
+void circularListPrint(struct CircularList* list);
+void circularListReverse(struct CircularList* list);
+
+// Deque interface
+
+void circularListAddFront(struct CircularList* list, TYPE value);
+void circularListAddBack(struct CircularList* list, TYPE value);
+TYPE circularListFront(struct CircularList* list);
+TYPE circularListBack(struct CircularList* list);
+void circularListRemoveFront(struct CircularList* list);
+void circularListRemoveBack(struct CircularList* list);
+int circularListIsEmpty(struct CircularList* list);
+
+#endif
