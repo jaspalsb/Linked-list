f938b6e9c9aa43a841380acfb6d5a8f258fd2a22
Jaspal Bainiwal <jbainiwal@gmail.com>
2018-09-04 20:11:17 -0700

    Create dequeMain.c

---
+++ dequeMain.c
@@ -0,0 +1,18 @@
+#include "dequeHeader.h"
+#include <stdio.h>
+
+int main()
+{	
+	struct CircularList* deque = circularListCreate(); 
+	circularListAddBack(deque, (TYPE)1);
+	circularListAddBack(deque, (TYPE)2);
+	circularListAddBack(deque, (TYPE)3);
+	circularListAddFront(deque, (TYPE)4);
+	circularListAddFront(deque, (TYPE)5);
+	circularListAddFront(deque, (TYPE)6);
+	circularListPrint(deque);
+	printf("%g\n", circularListFront(deque));
+	printf("%g\n", circularListBack(deque));
+	
+	return 0;
+}
